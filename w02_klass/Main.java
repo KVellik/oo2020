fb35d567d0ee8af2a339a49ddfeed3ac7f07f288
KVellik <wellik@tlu.ee>
2020-02-11 11:48:13 +0200

    Teine nädal

---
+++ w02_klass/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Character karl = new Character("Karl", CharacterType.WARRIOR);
+        karl.slogan = "Hey";
+
+        System.out.println(karl);
+    }      
+   
+}
